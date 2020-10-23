e4adc9c32c7dee253a103453bb2c436a2363c43d
bengbengku <anggiatbenget70@gmail.com>
2020-10-24 00:06:24 +0700

    MODIFY: button pada react-date-range

--- src/Banner.js
+++ src/Banner.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "./Banner.css";
 import { Button } from "@material-ui/core";
+import Search from "./Search";
 
 function Banner() {
   const [showSearch, setShowSearch] = useState(false);
@@ -8,8 +9,12 @@ function Banner() {
   return (
     <div className="banner">
       <div className="banner__search">
-        {showSearch && <h1>Show date picker</h1>}
-        <Button onClick={() => setShowSearch(!showSearch)} className="banner__searchButton" variant="outlined">
+        {showSearch && <Search />}
+        <Button
+          onClick={() => setShowSearch(!showSearch)}
+          className="banner__searchButton"
+          variant="outlined"
+        >
           Search Dates
         </Button>
       </div>
