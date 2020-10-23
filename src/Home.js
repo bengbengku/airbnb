8c20176fafa0464190cfcde7c4badea7713db26e
bengbengku <anggiatbenget70@gmail.com>
2020-10-24 01:44:08 +0700

    ADD: Create Card komponen dan tyling (Setup)

--- src/Home.js
+++ src/Home.js
@@ -5,8 +5,19 @@ import Banner from "./Banner";
 function Home() {
   return (
     <div className="home">
-      <h1>Home component</h1>
       <Banner />
+
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
+
+      <div className="home__section">
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
